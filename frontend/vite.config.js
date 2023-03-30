e4ecff31598aff63aabf3f3eeab42fb07bf3e9f1
kamyrdol32 <kamyrdol32@gmail.com>
2023-03-31 00:26:44 +0200

    Some updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         port: 3000,
         hmr: {
             host: 'kamilzeglen.pl',
-            port: 443,
+            port: 3000,
             protocol: 'wss',
         }
     }
